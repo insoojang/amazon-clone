6213da4f21b011e6e190598c71de08dc5a6d73a1
insoojang <insoo2846@gmail.com>
2022-12-09 11:47:08 +0900

    Add file firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: 'AIzaSyAQN6682JnHtleujq1LbQxaEwNuN90YgGY',
+    authDomain: 'test-b061c.firebaseapp.com',
+    projectId: 'test-b061c',
+    storageBucket: 'test-b061c.appspot.com',
+    messagingSenderId: '96138922982',
+    appId: '1:96138922982:web:8153ea68bde5f8ef5e2427',
+    measurementId: 'G-WQFB9QZXVD',
+}
