28e363fb003fd6fc30ba200cb96e4574d4df6e5b
insoojang <insoo2846@gmail.com>
2022-12-10 07:29:02 +0900

    Add Header layout

--- src/Header.js
+++ src/Header.js
@@ -1,30 +1,37 @@
 import React from 'react'
-// import '.Header.css'
+import './Header.css'
+import { Search as SearchIcon, ShoppingBasket } from '@mui/icons-material'
 
-const Header = (props) => {
+const Header = () => {
     return (
         <div className="header">
             <img
                 className="header_logo"
                 src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                alt="logo"
             />
-            <div className="header_search" type="text/">
-                <input />
+            <div className="header_search">
+                <input className="header_searchInput" type="text" />
+                <SearchIcon className="header_searchIcon" />
             </div>
 
             <div className="header_nav">
                 <div className="header_option">
-                    <span className="header_option">
-                        <div className="header_optionLineOne">안녕하세요!</div>
-                        <div className="header_optionLineTwo">로그인하기</div>
-                    </span>
-                    <span className="header_option">
-                        <div className="header_optionLineOne">돌아가기</div>
-                        <div className="header_optionLineTwo">주문내역</div>
-                    </span>
-                    <span className="header_option">
-                        <div className="header_optionLineOne">반가워요</div>
-                        <div className="header_optionLineTwo">구독과좋아요</div>
+                    <span className="header_optionLineOne">안녕하세요!</span>
+                    <span className="header_optionLineTwo">로그인하기</span>
+                </div>
+                <div className="header_option">
+                    <span className="header_optionLineOne">돌아가기</span>
+                    <span className="header_optionLineTwo">주문내역</span>
+                </div>
+                <div className="header_option">
+                    <span className="header_optionLineOne">반가워요!</span>
+                    <span className="header_optionLineTwo">구독과 좋아요</span>
+                </div>
+                <div className="header_optionBasket">
+                    <ShoppingBasket />
+                    <span className="header_optionLineTwoHeader_basketCount">
+                        0
                     </span>
                 </div>
             </div>
