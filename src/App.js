702e54e26585ded4301f880092c40a7b62b2e4b0
insoojang <insoo2846@gmail.com>
2022-12-12 14:27:47 +0900

    Add page Routing Logic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css'
 import Header from './Header'
 import Home from './Home'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
     return (
-        <div className="App">
-            <Header />
-            <Home />
-        </div>
+        <BrowserRouter>
+            <div className="App">
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />}></Route>
+                    <Route
+                        path="/checkout"
+                        element={<h1>장바구니 패이지 입니다</h1>}
+                    ></Route>
+                </Routes>
+            </div>
+        </BrowserRouter>
     )
 }
 
